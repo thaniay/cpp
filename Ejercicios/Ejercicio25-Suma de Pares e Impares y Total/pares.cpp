02639fa96551dc2a51cfbbfd5871949f2ec4f2e9
Thania Yanez <yorelyyanezguevara@gmail.com>
2020-07-26 16:33:41 -0600

    agregando ejercicio de pares, impares y total

--- Ejercicios/Ejercicio25-Suma de Pares e Impares y Total/pares.cpp
+++ Ejercicios/Ejercicio25-Suma de Pares e Impares y Total/pares.cpp
@@ -5,28 +5,18 @@ using namespace std;
 int main(int argc, char** argv) {
 	
     int pares = 0;
-    int impares= 0;
     
 	for (int i = 1; i <= 10; i++)
     {
         if (i % 2 == 0){
             pares = pares + i;
         }
-
-        if (i % 2 == 1){
-            impares = impares + i;
-        }
-
         cout << i << " ";
     }
 
     cout << endl;
     cout << endl;
-    cout << "Pares: " << pares ;
-    cout << endl;
-    cout << "Impares: " << impares;
-    cout << endl;
-    cout << "Total: " << pares + impares;
+    cout << "Total pares: " << pares ;
 
     return 0;
 }
