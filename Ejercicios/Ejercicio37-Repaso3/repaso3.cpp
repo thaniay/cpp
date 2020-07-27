eff48e592ffa6dc01d9efdd36a5d681e29cba0a2
Thania Yanez <yorelyyanezguevara@gmail.com>
2020-07-26 18:35:22 -0600

    agregando repaso 3

---
+++ Ejercicios/Ejercicio37-Repaso3/repaso3.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+   
+using namespace std;
+   
+int main(int argc, char** argv) {
+    
+    int a= 50;
+    int b= 10;
+    int c= 4;
+//  if (a > b && a > c){
+//    cout <<"a es mayor que b o mayor que c";
+//   }
+
+//  while (a > b){
+//          a++;
+
+//        if (a == 20){
+//            break;
+//         }
+            
+//    }
+
+        while (true){
+            while (true){
+                if (b == 10){
+                    break;
+                }
+                b++;
+            }
+            
+            if (a == 1)
+            {
+                break;
+            }
+            a++;
+        }
+        
+
+      return 0;
+
+    }
+    
+
+    
