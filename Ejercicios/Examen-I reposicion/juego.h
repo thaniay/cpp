5aeb013ecd589071355cf5d0d802d67b9a274246
Thania Yanez <yorelyyanezguevara@gmail.com>
2020-08-23 18:10:44 -0600

    agregando examen I reposicion

---
+++ Ejercicios/Examen-I reposicion/juego.h
@@ -0,0 +1,10 @@
+# include <iostream>
+
+using namespace std;
+
+void inicializarArreglo();
+void dificultad();
+void gotoxy(int x,int y);
+void pintar();
+bool gameover();
+void proceso(char &tecla, int &puntos);
